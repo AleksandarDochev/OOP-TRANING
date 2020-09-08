2c775a2bc01bb78477283a0a0e4910bbdbae3e33
Aleks <aleksandaredochev@gmail.com>
2020-09-08 13:08:40 +0300

    Second commit +instructions for git up to step 7

--- 1.py
+++ 1.py
@@ -1,12 +1,12 @@
 #Adding to git
-#1/Enter dir)      cd "/f/Pycharm/pycharm projects/OOP TRANING/git"
-#2int the folder ) git init
-#
-#
-#
-#
-#
-#
+#1/Enter dir)             cd "/f/Pycharm/pycharm projects/OOP TRANING/git"
+#2/int the folder )       git init
+#3/add the pythone file ) git add 1.py
+#4/do the first commit to local ) git commit -m "First commit"
+#5/add it to git hub repesitory ) git remote add origin remote-repository-URL
+#6/ verifi url )           git remote -v
+#7/Push the changes in your local repository to GitHub.) git push origin master
+
 
 
 
