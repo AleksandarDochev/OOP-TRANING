4687b6edb6d29ef968dbcdea2be378312db3666b
Aleks <aleksandaredochev@gmail.com>
2020-09-08 14:54:54 +0300

    just the 2 ways of class manual and automatic comparison

--- 1.py
+++ 1.py
@@ -9,8 +9,24 @@
 #8/$ git pull
 #9/Push the changes in your local repository to GitHub.) git push origin master
 
+ #TUTORIAL that I used https://www.youtube.com/watch?v=ZDa-Z5JzLYM&ab_channel=CoreySchafer
+###########################################################################
 class Employee:
-    pass
+#here def __init__(self): we star building the Mehod so we can properly use the
+#advantages of the class  ANd this how it lookks It creats it automaticly when we add the object Employee
+     def __init__(self, first, last , pay): #This is a METHOD /CONSTRUCTOR for universal ,
+              self.first  = first                            # Whe have self so it has it sticks to its name
+              self.last = last                               # and we set the arguments we want in the small brackets like
+              self.pay = pay
+              self.email = first + '.' + last + '@company.com'
+
+
+emp_1 = Employee('Corey', 'Schafer', 50000)  #we create the object and we pass in the same information but IN the correct order
+emp_2 = Employee('Test', 'User', 60000)
+
+
+
+#this is just a manual example of entering data that is unreliable and slow
 emp_1 = Employee()
 emp_1.first = 'Corey'  #This is instance variable
 emp_1.last = 'Shafer'   #This is instance variable
